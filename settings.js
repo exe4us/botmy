c6d3cf7c763af0d5748d9884aaa6b7447b454653
exe4us <101020078+exe4us@users.noreply.github.com>
2023-07-29 14:52:10 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.owner = ['919101765679'] //ur owner number
+global.ownernomer = "919101765679" //ur owner number2
+global.ownername = "god brutal" //ur owner name
+global.ytname = "YT: brutal" //ur yt chanel name
+global.socialm = "GitHub: brutal" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.botname = "brutal"
+global.ownernumber = '919101765679'
+global.ownername = 'brutal'
+global.ownerNumber = ["919101765679@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@brutal"
+global.websitex = "https://youtu.be/brutal"
+global.wagc = "https://chat.whatsapp.com/"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
+global.wm = "brutal Inc."
+global.botscript = 'https://github.com/brutal' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "brutal\n\n"
+global.creator = "919101765679@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
